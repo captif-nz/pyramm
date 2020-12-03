fc32a8cebc1f769a84d4e03b4c5590700cd96b6d
John Bull <johnbullnz@gmail.com>
2020-12-04 10:11:53 +1300

    change to pyproject.toml

--- pyramm/__init__.py
+++ pyramm/__init__.py
@@ -1,3 +1,17 @@
-__version__ = 1.2
+__version__ = 1.3
 
-from . import api  # noqa
+from pyramm import api  # noqa
+
+
+if __name__ == "__main__":
+    # Connect and build centreline object:
+    conn = api.Connection()
+    centreline = conn.centreline()
+
+    # Download the surface table
+    top_surface = conn.top_surface().reset_index()
+
+    # Append geometry:
+    top_surface = centreline.append_geometry(top_surface)
+
+    top_surface
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,4 +2,4 @@ import pyramm
 
 
 def test_version():
-    assert pyramm.__version__ == "1.2"
+    assert pyramm.__version__ == "1.3"
