dac2a86afd60c6fe8d9593e7c3a5e52330931349
John Bull <johnbullnz@gmail.com>
2021-07-15 13:19:01 +1200

    fix warning

--- pyramm/ops/top_surface.py
+++ pyramm/ops/top_surface.py
@@ -118,7 +118,7 @@ def _surface_records_to_grid(df):
 def _extract_surface_records_from_grid(grid):
     groups = (
         (np.insert(np.diff(grid), 0, 1 if grid[0] > 0 else 0) != 0)
-        .astype(np.int)
+        .astype(int)
         .cumsum()
     )
     surfaces_dict = defaultdict(list)
