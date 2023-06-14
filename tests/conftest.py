d0d9d8f088fe993bbf46a9d4daf713bb97b41b2e
John <johnbullnz@gmail.com>
2023-06-15 11:59:18 +1200

    use session scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,21 +3,21 @@ import pytest
 from pyramm.api import Connection
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def conn():
     return Connection()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def centreline(conn):
     return conn.centreline()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def roadnames(conn):
     return conn.roadnames()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def top_surface(conn):
     return conn.top_surface()
