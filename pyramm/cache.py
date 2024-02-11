1a25c6f4e3fd914a222f529ba4571f4e128449a8
John <johnbullnz@gmail.com>
2024-02-12 10:26:42 +1300

    use database name in cache filename

--- pyramm/cache.py
+++ pyramm/cache.py
@@ -3,6 +3,7 @@ import pickle
 
 import pandas as pd
 
+from copy import copy
 from datetime import datetime
 from frozendict import frozendict
 from functools import wraps
@@ -31,13 +32,22 @@ def generate_cache_file_path(name=None, func_args=[], func_kwargs={}):
     if name is not None:
         prefix.append(name)
 
-    func_args = func_args[1:] if len(func_args) > 1 else []
+    args = [copy(vv) for vv in func_args]
+    kwargs = {kk: copy(vv) for kk, vv in func_kwargs.items()}
+
+    if len(args) > 0:
+        # If the first argument is a Connection object, use the database name
+        # as part of the cache file name and remove the Connection object from
+        # the list of arguments:
+        if type(args[0]).__name__ == "Connection":
+            kwargs["database"] = args[0].database
+            args = args[1:]
 
     return TEMP_DIRECTORY.joinpath(
         "_".join(
             prefix
-            + [str(vv) for vv in func_args]
-            + [str(vv) for vv in func_kwargs.values()]
+            + [str(vv) for vv in args]
+            + [str(vv) for vv in kwargs.values()]
             + [pd.__version__]
             + [__version__]
         )
