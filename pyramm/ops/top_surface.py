6661df06dcafec7205d3455f247b60bb0b4597e2
John <johnbullnz@gmail.com>
2024-12-03 01:17:47 +0000

    build_top_surface back

--- pyramm/ops/top_surface.py
+++ pyramm/ops/top_surface.py
@@ -1,95 +1,95 @@
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 
-# from pyramm.tables import TopSurface
-# from pyramm.helpers import _extract_records_from_grid, _records_to_grid
-
-
-# def build_top_surface(tables: List[pd.DataFrame]) -> pd.DataFrame:
-#     """
-#     Generate the top_surface table from several RAMM-style surface tables (with
-#     overlapping surface records. If the table contains "full_width_flag" only full width
-#     surface records are kept.
-
-#     Surfaces are chosen in the following order: "surface_date", "start_m", "end_m". I.e.
-#     Newer surfaces will take priority, followed by surfaces with a higher "start_m" (if
-#     "surface_date" is the same), followed by surfaces with a higher "end_m" (if
-#     "surface_date" and "start_m" are the same).
-
-#     :param tables: List of DataFrames containing RAMM-style surface tables.
-#     :return: new top_surface table with non-overlapping surface records.
-
-#     """
-#     tables = [TopSurface.from_frame(tt) for tt in tables]
-#     tables = [_reset_surface_table_index(tt) for tt in tables]
-#     tables = [_limit_to_full_width(tt) for tt in tables]
-
-#     df = pd.concat(tables, ignore_index=True)
-#     top_surface = pd.DataFrame()
-#     for _, gg in df.groupby("road_id"):
-#         gg = gg.sort_values(["surface_date", "start_m", "end_m"]).reset_index(drop=True)
-#         gg.index += 1
-#         grid = _records_to_grid(gg)
-#         surfaces = _extract_records_from_grid(grid).rename(columns={"id": "surface_id"})
-#         surfaces = surfaces.join(
-#             gg[[cc for cc in gg.columns if cc not in ["start_m", "end_m"]]],
-#             on="surface_id",
-#         ).drop(columns="surface_id")
-#         top_surface = pd.concat([top_surface, surfaces], ignore_index=True)
-#     return top_surface.set_index(["road_id", "start_m", "end_m"]).sort_index()
+from pyramm.tables import TopSurface
+from pyramm.helpers import _extract_records_from_grid, _records_to_grid
 
 
-# def append_surface_details_to_segments(df: pd.DataFrame, top_surface: pd.DataFrame):
-#     """
-#     Append surface details to each road segment.
+def build_top_surface(tables: list[pd.DataFrame]) -> pd.DataFrame:
+    """
+    Generate the top_surface table from several RAMM-style surface tables (with
+    overlapping surface records. If the table contains "full_width_flag" only full width
+    surface records are kept.
 
-#     :param df: Road segments. Must contain "road_id", "start_m", "end_m" columns.
-#     :param top_surface: RAMM-style top_surface table with non-overlapping surface records.
-#     :return: Road segments with surfaces details appended.
+    Surfaces are chosen in the following order: "surface_date", "start_m", "end_m". I.e.
+    Newer surfaces will take priority, followed by surfaces with a higher "start_m" (if
+    "surface_date" is the same), followed by surfaces with a higher "end_m" (if
+    "surface_date" and "start_m" are the same).
 
-#     """
-#     top_surface = TopSurface.from_frame(top_surface)
-#     top_surface = _reset_surface_table_index(top_surface)
+    :param tables: List of DataFrames containing RAMM-style surface tables.
+    :return: new top_surface table with non-overlapping surface records.
 
-#     surface_columns = [
-#         cc for cc in top_surface.columns if cc not in ["road_id", "start_m", "end_m"]
-#     ]
-#     df[surface_columns] = None
+    """
+    tables = [TopSurface.from_frame(tt) for tt in tables]
+    tables = [_reset_surface_table_index(tt) for tt in tables]
+    tables = [_limit_to_full_width(tt) for tt in tables]
 
-#     valid_surfaces = top_surface.loc[top_surface["road_id"].isin(df["road_id"])]
-#     if len(valid_surfaces) == 0:
-#         return df
-
-#     for _, row in valid_surfaces.iterrows():
-#         # Limit to the road segments that are fully within the surface section.
-#         # Using less / greater than ensures that a join on the boundary results in
-#         # the two straddling segments being excluded.
-#         selected_segments = df.loc[
-#             (df["road_id"] == row["road_id"])
-#             & (df["start_m"] > row["start_m"])
-#             & (df["end_m"] < row["end_m"])
-#         ]
-
-#         # Append the surface details to the appropriate rows:
-#         for cc in surface_columns:
-#             df.loc[selected_segments.index, cc] = row[cc]
-
-#     df = _fix_na_columns(df)
-#     return df
+    df = pd.concat(tables, ignore_index=True)
+    top_surface = pd.DataFrame()
+    for _, gg in df.groupby("road_id"):
+        gg = gg.sort_values(["surface_date", "start_m", "end_m"]).reset_index(drop=True)
+        gg.index += 1
+        grid = _records_to_grid(gg)
+        surfaces = _extract_records_from_grid(grid).rename(columns={"id": "surface_id"})
+        surfaces = surfaces.join(
+            gg[[cc for cc in gg.columns if cc not in ["start_m", "end_m"]]],
+            on="surface_id",
+        ).drop(columns="surface_id")
+        top_surface = pd.concat([top_surface, surfaces], ignore_index=True)
+    return top_surface.set_index(["road_id", "start_m", "end_m"]).sort_index()
 
 
-# def _limit_to_full_width(df):
-#     if "full_width_flag" not in df.columns:
-#         return df
-#     return df.loc[df["full_width_flag"].str.lower() == "y"]
+def append_surface_details_to_segments(df: pd.DataFrame, top_surface: pd.DataFrame):
+    """
+    Append surface details to each road segment.
+
+    :param df: Road segments. Must contain "road_id", "start_m", "end_m" columns.
+    :param top_surface: RAMM-style top_surface table with non-overlapping surface records.
+    :return: Road segments with surfaces details appended.
+
+    """
+    top_surface = TopSurface.from_frame(top_surface)
+    top_surface = _reset_surface_table_index(top_surface)
+
+    surface_columns = [
+        cc for cc in top_surface.columns if cc not in ["road_id", "start_m", "end_m"]
+    ]
+    df[surface_columns] = None
+
+    valid_surfaces = top_surface.loc[top_surface["road_id"].isin(df["road_id"])]
+    if len(valid_surfaces) == 0:
+        return df
+
+    for _, row in valid_surfaces.iterrows():
+        # Limit to the road segments that are fully within the surface section.
+        # Using less / greater than ensures that a join on the boundary results in
+        # the two straddling segments being excluded.
+        selected_segments = df.loc[
+            (df["road_id"] == row["road_id"])
+            & (df["start_m"] > row["start_m"])
+            & (df["end_m"] < row["end_m"])
+        ]
+
+        # Append the surface details to the appropriate rows:
+        for cc in surface_columns:
+            df.loc[selected_segments.index, cc] = row[cc]
+
+    df = _fix_na_columns(df)
+    return df
 
 
-# def _reset_surface_table_index(df):
-#     return df.reset_index() if df.index.names != [None] else df
+def _limit_to_full_width(df):
+    if "full_width_flag" not in df.columns:
+        return df
+    return df.loc[df["full_width_flag"].str.lower() == "y"]
 
 
-# def _fix_na_columns(df):
-#     for cc in df.columns:
-#         fill_value = 0 if df[cc].dtype.type == np.float64 else ""
-#         df[cc].fillna(fill_value, inplace=True)
-#     return df
+def _reset_surface_table_index(df):
+    return df.reset_index() if df.index.names != [None] else df
+
+
+def _fix_na_columns(df):
+    for cc in df.columns:
+        fill_value = 0 if df[cc].dtype.type == np.float64 else ""
+        df[cc].fillna(fill_value, inplace=True)
+    return df
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,251 +1,251 @@
-# import pytest
-# import pandas as pd
+import pytest
+import pandas as pd
 
-# from pandas import Timestamp
-# from datetime import date
+from pandas import Timestamp
+from datetime import date
 
-# from pyramm.ops.top_surface import build_top_surface, append_surface_details_to_segments
-
-
-# @pytest.fixture
-# def original():
-#     return pd.DataFrame.from_records(
-#         [
-#             {
-#                 "road_id": 100,
-#                 "start_m": 20,
-#                 "end_m": 200,
-#                 "surface_date": date(2020, 1, 1),
-#                 "name": "A",
-#             },
-#             {
-#                 "road_id": 100,
-#                 "start_m": 20,
-#                 "end_m": 60,
-#                 "surface_date": date(2020, 1, 1),
-#                 "name": "A",
-#             },
-#             {
-#                 "road_id": 101,
-#                 "start_m": 230,
-#                 "end_m": 500,
-#                 "surface_date": date(2021, 1, 1),
-#                 "name": "B",
-#             },
-#             {
-#                 "road_id": 101,
-#                 "start_m": 230,
-#                 "end_m": 260,
-#                 "surface_date": date(2021, 1, 1),
-#                 "name": "C",
-#             },
-#             {
-#                 "road_id": 102,
-#                 "start_m": 0,
-#                 "end_m": 9999,
-#                 "surface_date": date(2018, 6, 6),
-#                 "name": "D",
-#             },
-#         ]
-#     ).set_index(["road_id", "start_m", "end_m"])
+from pyramm.ops.top_surface import build_top_surface, append_surface_details_to_segments
 
 
-# class TestTopSurface:
-#     class TestBuildTopSurface:
-#         def test_build_top_surface(self, original):
-#             index_names = ["road_id", "start_m", "end_m"]
-#             custom = pd.DataFrame.from_records(
-#                 [
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 0,
-#                         "end_m": 30,
-#                         "surface_date": date(2020, 1, 2),
-#                         "name": "1",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 20,
-#                         "end_m": 30,
-#                         "surface_date": date(2019, 12, 1),
-#                         "name": "2",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 60,
-#                         "end_m": 100,
-#                         "surface_date": date(2020, 1, 5),
-#                         "name": "3",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 190,
-#                         "end_m": 220,
-#                         "surface_date": date(2020, 1, 10),
-#                         "name": "4",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 70,
-#                         "end_m": 90,
-#                         "surface_date": date(2020, 1, 5),
-#                         "name": "5",
-#                     },
-#                 ]
-#             )
-#             new = build_top_surface([original, custom])
-#             expected = (
-#                 pd.DataFrame.from_records(
-#                     [
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 0,
-#                             "end_m": 30,
-#                             "surface_date": Timestamp(2020, 1, 2),
-#                             "name": "1",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 30,
-#                             "end_m": 60,
-#                             "surface_date": Timestamp(2020, 1, 1),
-#                             "name": "A",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 60,
-#                             "end_m": 70,
-#                             "surface_date": Timestamp(2020, 1, 5),
-#                             "name": "3",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 70,
-#                             "end_m": 90,
-#                             "surface_date": Timestamp(2020, 1, 5),
-#                             "name": "5",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 90,
-#                             "end_m": 100,
-#                             "surface_date": Timestamp(2020, 1, 5),
-#                             "name": "3",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 100,
-#                             "end_m": 190,
-#                             "surface_date": Timestamp(2020, 1, 1),
-#                             "name": "A",
-#                         },
-#                         {
-#                             "road_id": 100,
-#                             "start_m": 190,
-#                             "end_m": 220,
-#                             "surface_date": Timestamp(2020, 1, 10),
-#                             "name": "4",
-#                         },
-#                         {
-#                             "road_id": 101,
-#                             "start_m": 230,
-#                             "end_m": 500,
-#                             "surface_date": Timestamp(2021, 1, 1),
-#                             "name": "B",
-#                         },
-#                         {
-#                             "road_id": 102,
-#                             "start_m": 0,
-#                             "end_m": 9999,
-#                             "surface_date": Timestamp(2018, 6, 6),
-#                             "name": "D",
-#                         },
-#                     ]
-#                 )
-#                 .set_index(index_names)
-#                 .sort_index()
-#             )
-#             assert new.equals(expected)
+@pytest.fixture
+def original():
+    return pd.DataFrame.from_records(
+        [
+            {
+                "road_id": 100,
+                "start_m": 20,
+                "end_m": 200,
+                "surface_date": date(2020, 1, 1),
+                "name": "A",
+            },
+            {
+                "road_id": 100,
+                "start_m": 20,
+                "end_m": 60,
+                "surface_date": date(2020, 1, 1),
+                "name": "A",
+            },
+            {
+                "road_id": 101,
+                "start_m": 230,
+                "end_m": 500,
+                "surface_date": date(2021, 1, 1),
+                "name": "B",
+            },
+            {
+                "road_id": 101,
+                "start_m": 230,
+                "end_m": 260,
+                "surface_date": date(2021, 1, 1),
+                "name": "C",
+            },
+            {
+                "road_id": 102,
+                "start_m": 0,
+                "end_m": 9999,
+                "surface_date": date(2018, 6, 6),
+                "name": "D",
+            },
+        ]
+    ).set_index(["road_id", "start_m", "end_m"])
 
-#         @pytest.mark.skip(
-#             reason="top_surface table no longer contains full_width_flag = Y"
-#         )
-#         def test_build_top_surface_real_table(self, top_surface):
-#             top_surface_limited = top_surface.iloc[:100]
-#             new = build_top_surface([top_surface_limited])
-#             expected = top_surface_limited.loc[
-#                 top_surface_limited["full_width_flag"].str.lower() == "y"
-#             ]
-#             assert new.equals(expected)
 
-#     class TestAppendSurfaceDetailsToSegments:
-#         def test_append_surface_details_to_segments(self, original):
-#             df = pd.DataFrame.from_records(
-#                 [
-#                     {"road_id": 99, "start_m": 0, "end_m": 20},
-#                     {"road_id": 100, "start_m": 0, "end_m": 20},
-#                     {"road_id": 100, "start_m": 20, "end_m": 40},
-#                     {"road_id": 100, "start_m": 40, "end_m": 60},
-#                     {"road_id": 101, "start_m": 200, "end_m": 220},
-#                     {"road_id": 101, "start_m": 220, "end_m": 240},
-#                     {"road_id": 101, "start_m": 240, "end_m": 260},
-#                 ]
-#             )
-#             original = build_top_surface(
-#                 [original]
-#             )  # Removes overlapping surface records
-#             df = append_surface_details_to_segments(df, original)
-#             expected = df = pd.DataFrame.from_records(
-#                 [
-#                     {
-#                         "road_id": 99,
-#                         "start_m": 0,
-#                         "end_m": 20,
-#                         "surface_date": "",
-#                         "name": "",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 0,
-#                         "end_m": 20,
-#                         "surface_date": "",
-#                         "name": "",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 20,
-#                         "end_m": 40,
-#                         "surface_date": Timestamp(2020, 1, 1),
-#                         "name": "A",
-#                     },
-#                     {
-#                         "road_id": 100,
-#                         "start_m": 40,
-#                         "end_m": 60,
-#                         "surface_date": Timestamp(2020, 1, 1),
-#                         "name": "A",
-#                     },
-#                     {
-#                         "road_id": 101,
-#                         "start_m": 200,
-#                         "end_m": 220,
-#                         "surface_date": "",
-#                         "name": "",
-#                     },
-#                     {
-#                         "road_id": 101,
-#                         "start_m": 220,
-#                         "end_m": 240,
-#                         "surface_date": "",
-#                         "name": "",
-#                     },
-#                     {
-#                         "road_id": 101,
-#                         "start_m": 240,
-#                         "end_m": 260,
-#                         "surface_date": Timestamp(2021, 1, 1),
-#                         "name": "C",
-#                     },
-#                 ]
-#             )
-#             assert df.equals(expected)
+class TestTopSurface:
+    class TestBuildTopSurface:
+        def test_build_top_surface(self, original):
+            index_names = ["road_id", "start_m", "end_m"]
+            custom = pd.DataFrame.from_records(
+                [
+                    {
+                        "road_id": 100,
+                        "start_m": 0,
+                        "end_m": 30,
+                        "surface_date": date(2020, 1, 2),
+                        "name": "1",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 20,
+                        "end_m": 30,
+                        "surface_date": date(2019, 12, 1),
+                        "name": "2",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 60,
+                        "end_m": 100,
+                        "surface_date": date(2020, 1, 5),
+                        "name": "3",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 190,
+                        "end_m": 220,
+                        "surface_date": date(2020, 1, 10),
+                        "name": "4",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 70,
+                        "end_m": 90,
+                        "surface_date": date(2020, 1, 5),
+                        "name": "5",
+                    },
+                ]
+            )
+            new = build_top_surface([original, custom])
+            expected = (
+                pd.DataFrame.from_records(
+                    [
+                        {
+                            "road_id": 100,
+                            "start_m": 0,
+                            "end_m": 30,
+                            "surface_date": Timestamp(2020, 1, 2),
+                            "name": "1",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 30,
+                            "end_m": 60,
+                            "surface_date": Timestamp(2020, 1, 1),
+                            "name": "A",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 60,
+                            "end_m": 70,
+                            "surface_date": Timestamp(2020, 1, 5),
+                            "name": "3",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 70,
+                            "end_m": 90,
+                            "surface_date": Timestamp(2020, 1, 5),
+                            "name": "5",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 90,
+                            "end_m": 100,
+                            "surface_date": Timestamp(2020, 1, 5),
+                            "name": "3",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 100,
+                            "end_m": 190,
+                            "surface_date": Timestamp(2020, 1, 1),
+                            "name": "A",
+                        },
+                        {
+                            "road_id": 100,
+                            "start_m": 190,
+                            "end_m": 220,
+                            "surface_date": Timestamp(2020, 1, 10),
+                            "name": "4",
+                        },
+                        {
+                            "road_id": 101,
+                            "start_m": 230,
+                            "end_m": 500,
+                            "surface_date": Timestamp(2021, 1, 1),
+                            "name": "B",
+                        },
+                        {
+                            "road_id": 102,
+                            "start_m": 0,
+                            "end_m": 9999,
+                            "surface_date": Timestamp(2018, 6, 6),
+                            "name": "D",
+                        },
+                    ]
+                )
+                .set_index(index_names)
+                .sort_index()
+            )
+            assert new.equals(expected)
+
+        @pytest.mark.skip(
+            reason="top_surface table no longer contains full_width_flag = Y"
+        )
+        def test_build_top_surface_real_table(self, top_surface):
+            top_surface_limited = top_surface.iloc[:100]
+            new = build_top_surface([top_surface_limited])
+            expected = top_surface_limited.loc[
+                top_surface_limited["full_width_flag"].str.lower() == "y"
+            ]
+            assert new.equals(expected)
+
+    class TestAppendSurfaceDetailsToSegments:
+        def test_append_surface_details_to_segments(self, original):
+            df = pd.DataFrame.from_records(
+                [
+                    {"road_id": 99, "start_m": 0, "end_m": 20},
+                    {"road_id": 100, "start_m": 0, "end_m": 20},
+                    {"road_id": 100, "start_m": 20, "end_m": 40},
+                    {"road_id": 100, "start_m": 40, "end_m": 60},
+                    {"road_id": 101, "start_m": 200, "end_m": 220},
+                    {"road_id": 101, "start_m": 220, "end_m": 240},
+                    {"road_id": 101, "start_m": 240, "end_m": 260},
+                ]
+            )
+            original = build_top_surface(
+                [original]
+            )  # Removes overlapping surface records
+            df = append_surface_details_to_segments(df, original)
+            expected = df = pd.DataFrame.from_records(
+                [
+                    {
+                        "road_id": 99,
+                        "start_m": 0,
+                        "end_m": 20,
+                        "surface_date": "",
+                        "name": "",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 0,
+                        "end_m": 20,
+                        "surface_date": "",
+                        "name": "",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 20,
+                        "end_m": 40,
+                        "surface_date": Timestamp(2020, 1, 1),
+                        "name": "A",
+                    },
+                    {
+                        "road_id": 100,
+                        "start_m": 40,
+                        "end_m": 60,
+                        "surface_date": Timestamp(2020, 1, 1),
+                        "name": "A",
+                    },
+                    {
+                        "road_id": 101,
+                        "start_m": 200,
+                        "end_m": 220,
+                        "surface_date": "",
+                        "name": "",
+                    },
+                    {
+                        "road_id": 101,
+                        "start_m": 220,
+                        "end_m": 240,
+                        "surface_date": "",
+                        "name": "",
+                    },
+                    {
+                        "road_id": 101,
+                        "start_m": 240,
+                        "end_m": 260,
+                        "surface_date": Timestamp(2021, 1, 1),
+                        "name": "C",
+                    },
+                ]
+            )
+            assert df.equals(expected)
